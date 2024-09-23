61e0ef70dfd66f99fdccee31a471c82548128cd4
Emilio Velasco <emilio_785@hotmail.com>
2024-09-23 17:14:20 -0600

    create my org

---
+++ middleware.ts
@@ -0,0 +1,20 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
+
+// This Middleware does not protect any routes by default.
+// See https://clerk.com/docs/references/nextjs/clerk-middleware for more information about configuring your Middleware
+export default clerkMiddleware((auth, request) => {
+  if (!isPublicRoute(request)) {
+    auth().protect();
+  }
+});
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
+};
